fe158a27be6db49c9b1ab4b6e0dd7746f6e31926
waiga <hyang9387@gmail.com>
2022-04-11 10:24:10 +0900

    Initial Commit

---
+++ WaveUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WaveUI
+//
+//  Created by 김미향 on 2022/04/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WaveUI/WaveUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaveUIApp.swift
+//  WaveUI
+//
+//  Created by 김미향 on 2022/04/11.
+//
+
+import SwiftUI
+
+@main
+struct WaveUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
